71d93d30f486af18e013de04ac6136850dd89f72
Denis <denis.snagovskiy@list.ru>
2021-12-24 18:18:29 +0300

    Amend allowed hosts

--- yaus/settings.py
+++ yaus/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yaus-resume']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yaus-resume.herokuapp.com']
 
 
 # Application definition
